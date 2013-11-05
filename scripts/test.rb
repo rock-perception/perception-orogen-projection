25ea428145d0c7d48a1a8ab1b27b4d0af45249a1
Jakob Schwendner <jakob@jschwendner-lap.(none)>
2013-11-05 15:41:36 +0100

    updated script to also replay logs

--- scripts/test.rb
+++ scripts/test.rb
@@ -1,14 +1,26 @@
 require 'vizkit'
 require 'rock/bundle'
 require 'readline'
+include Orocos
 
 Bundles.initialize
 Bundles.transformer.load_conf(Bundles.find_file('config', 'transforms_scripts.rb'))
 
+log = nil
+if !ARGV.empty?
+    log = Log::Replay.open ARGV
+end
+
 Bundles.run "projection::ColorizePointcloud" => "colorize", "output" => nil do
-    Orocos.name_service << Orocos::CORBA::NameService.new("192.168.56.101")
-    tilt_scan = Orocos.name_service.get "tilt_scan_front"
-    camera = Orocos.name_service.get "camera"
+    tilt_scan = camera = nil
+    if log
+	tilt_scan = log.tilt_scan_front
+	camera = log.camera
+    else
+	Orocos.name_service << Orocos::CORBA::NameService.new("192.168.56.101")
+	tilt_scan = Orocos.name_service.get "tilt_scan_front"
+	camera = Orocos.name_service.get "camera"
+    end
 
     col = Orocos.name_service.get "colorize"
     col.transformer_max_latency = 2.0
@@ -24,6 +36,11 @@ Bundles.run "projection::ColorizePointcloud" => "colorize", "output" => nil do
     col.start
 
     # wait for finish
-    Readline.readline "Press RETURN to exit."
+    if log
+	Vizkit.control log
+	Vizkit.exec
+    else
+	Readline.readline "Press RETURN to exit."
+    end
 end
 
